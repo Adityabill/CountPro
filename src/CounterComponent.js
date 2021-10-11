bd11e8f1487a88b4b0f760cd7303a08792f92e39
Adityabill <adityabikrambill@gmail.com>
2021-10-11 18:52:25 +0530

    Created 'CounterComponent' component

---
+++ src/CounterComponent.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export default class CounterComponents extends React.Component{
+    constructor(){
+        super();
+
+    }
+
+    render(){
+        return(
+            <div>
+                <div>
+                 <h1>
+                     Counter Pro
+                 </h1>
+                 </div>
+
+                 <div>
+                     
+                 </div>
+
+
+            </div>
+        );
+        
+    }
+}
